483761551f58262e4d41b1fa5198b3a9322f800f
JericHunter <jeric.hunterr@gmail.com>
2021-05-13 02:02:02 -0700

    Implemented email

--- server.js
+++ server.js
@@ -11,7 +11,73 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override')
 
+const nodemailer = require('nodemailer');
+const mg = require('nodemailer-mailgun-transport');
+
+
+const auth = {
+  auth: {
+    api_key: process.env.MAILGUN_API_KEY,
+    domain: process.env.EMAIL_DOMAIN
+  }
+}
+const nodemailerMailgun = nodemailer.createTransport(mg(auth));
 const app = express();
+
+
+// export our send mail function
+module.exports.sendMail = (user, req, res) => {
+  // send an email to the user's email with a provided template
+  nodemailerMailgun.sendMail({
+      from: 'no-reply@example.com',
+      to: user.email, // An array if you have multiple recipients.
+      subject: 'Pet Purchased!',
+      template: {
+          name: 'email.handlebars',
+          engine: 'handlebars',
+          context: user
+      }
+  // One mail is sent, redirect to the purchased pet's page
+  }).then(info => {
+      console.log('Response: ' + info);
+      res.redirect(`/pets/${req.params.id}`);
+  // Catch error and redirect to the purchased pet's page
+  }).catch(err => {
+      console.log('Error: ' + err);
+      res.redirect(`/pets/${req.params.id}`);
+  });
+}
+
+
+
+const user = {
+  email: 'shaan.hurley25@gmail.com',
+  name: 'Shaan',
+  age: '27'
+};
+
+
+nodemailerMailgun.sendMail({
+  from: 'no-reply@example.com',
+  to: user.email, // An array if you have multiple recipients.
+  subject: 'Hey you, awesome!',
+  template: {
+    name: 'email.handlebars',
+    engine: 'handlebars',
+    context: user
+  }
+}).then(info => {
+  console.log('Response: ' + info);
+}).catch(err => {
+  console.log('Error: ' + err);
+});
+
+
+
+
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/petes-pets');
 
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/petes-pets');
